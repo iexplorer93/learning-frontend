44469fe26390083cb0842742c2316ac63d215e78
Виктор <iexplorer93@MacBook-Pro-Viktor.local>
2026-03-02 20:48:28 +0500

    Начал делать popup

---
+++ Metrica/js/popup.js
@@ -0,0 +1,24 @@
+const POPUP_OPENED_CLASSNAME = 'popup-visible';
+const BODY_FIXED_CLASSNAME = 'body_fixed';
+
+const bodyNode = document.querySelector('body');
+const popupNode = document.querySelector('.popup');
+const btnOpenNode = document.querySelector('.services-section__btn');
+const popupContentNode = document.querySelector('.popup__content')
+const btnCloseNode = document.querySelector('.popup__close-btn');
+
+btnOpenNode.addEventListener('click', togglePopup);
+btnCloseNode.addEventListener('click', togglePopup);
+
+popupNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
+
+    if (isClickOutsideContent) {
+        togglePopup();
+    }
+})
+
+function togglePopup() {
+    popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
+    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+}
