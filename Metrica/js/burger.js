0c9e9c95c4fcd03f08443ae0c15ffbbdfeabb619
Виктор <iexplorer93@MacBook-Pro-Viktor.local>
2026-03-04 21:52:42 +0500

    Закончил сайт. чек поинт 4

--- Metrica/js/burger.js
+++ Metrica/js/burger.js
@@ -1,11 +1,12 @@
 const BURGER_OPENED_CLASSNAME = 'burger__open';
-const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn__open';
 const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
+const BURGER_BTN_CLASSNAME = 'burger-btn--active';
 
 const bodyNode_2 = document.querySelector('body');
 const burgerNode = document.querySelector('.burger');
 const burgerBtnNode = document.querySelector('.burger-btn');
 const burgerContentNode = document.querySelector('.burger__content')
+const burgerNav = document.querySelector(".burger__nav");
 
 burgerBtnNode.addEventListener('click', toggleBurger);
 
@@ -17,8 +18,20 @@ burgerNode.addEventListener('click', (event) => {
     }
 })
 
+burgerNav.addEventListener("click", (e) => {
+  const link = e.target.closest("a");
+  if (!link) return;
+  closeBurger();
+})
+
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
     bodyNode_2.classList.toggle(BODY_FIXED_CLASSNAME_2);
-    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+    burgerBtnNode.classList.toggle(BURGER_BTN_CLASSNAME);
+}
+
+function closeBurger() {
+  burgerNode.classList.remove(BURGER_OPENED_CLASSNAME);
+  burgerBtnNode.classList.remove(BURGER_BTN_CLASSNAME);
+  bodyNode_2.classList.remove(BODY_FIXED_CLASSNAME_2);
 }
